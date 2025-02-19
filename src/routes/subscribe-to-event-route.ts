656a73b2a54d32a618062d0e5f3e9cf52fbe19d1
Luciano-Junior <tec.luciano.ti@gmail.com>
2025-02-18 23:16:35 -0300

    Add config do Banco de Dados + criação da primeira tabela

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,32 +1,35 @@
-import {z} from "zod"
-import { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
+import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
 
-export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) =>{
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
+  app.post(
+    '/subscriptions',
+    {
+      schema: {
+        summary: 'Subscription someone to the event',
+        tags: ['Subscription'],
+        body: z.object({
+          //https://zod.dev/
+          name: z.string(),
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.object({
+            name: z.string(),
+            email: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name, email } = request.body
 
-    app.post('/subscriptions',{
-        schema:{
-            summary: "Subscription someone to the event",
-            tags:["Subscription"],
-            body: z.object({//https://zod.dev/
-                name: z.string(),
-                email: z.string().email()
-            }),
-            response: {
-                201: z.object({
-                    name: z.string(),
-                    email: z.string()
-                })
-            }
-        }
-    }, async (request, reply)=>{
-        const {name, email} = request.body
-    
-        // criação da inscrição no banco de dados
-    
-        return reply.status(201).send({
-            name,
-            email
-        })
-    })
+      // criação da inscrição no banco de dados
 
+      return reply.status(201).send({
+        name,
+        email,
+      })
+    }
+  )
 }
